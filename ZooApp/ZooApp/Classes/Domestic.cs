dc18c7c93040784a41b81392a74c54b3db7ae2dc
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-24 20:37:36 -0700

    reset with simpler class structure

---
+++ ZooApp/ZooApp/Classes/Domestic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooApp.Classes
+{
+    abstract class Domestic : Animal
+    {
+
+    }
+}
---
+++ ZooApp/ZooApp/Classes/Fantasy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooApp.Classes
+{
+    abstract class Fantasy : Animal
+    {
+    }
+}
