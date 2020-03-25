dae38d753ada4ffd6fe1bc926a6f9673cbbdb076
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-25 00:05:52 -0700

    adds some demonstrations in program file

--- ZooApp/ZooApp/Program.cs
+++ ZooApp/ZooApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ZooApp.Classes;
 
 namespace ZooApp
 {
@@ -6,7 +7,67 @@ namespace ZooApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProveAnimalBehavior();
+        }
+        static void ProveAnimalBehavior()
+        {
+            // Use polymorphism to create base class arrays with concrete class objects
+            Animal[] zooPopulation = new Animal[]
+            {
+                new Snake(),
+                new Crocodile(),
+                new Bear(),
+                new Cat("Carl"),
+                new Horse("Harold"),
+                new Dragon(),
+                new Phoenix()
+            };
+
+            Reptile[] slithers = new Reptile[2];
+            Domestic[] pets = new Domestic[2];
+
+            // Demonstrate Animal Methods
+            Console.WriteLine("All Animals Make Noise, and Like to eat!");
+            foreach (Animal creature in zooPopulation)
+            {
+                Console.WriteLine("Press a key to hear the next one...");
+                Console.ReadLine();
+                creature.MakeNoise();
+                creature.Eat();
+                Console.WriteLine();
+            }
+
+            // Explicitly cast Animals to Wild type array
+            Wild[] wilds = new Wild[]
+            {
+                (Wild)zooPopulation[0],
+                (Wild)zooPopulation[1],
+                (Wild)zooPopulation[2]
+            };
+
+            // Demonstrate Wild Method
+            foreach (Wild creature in wilds)
+                creature.Hunt((Wild)zooPopulation[1]);
+
+            // Demonstrate Mammal and Reptile Methods
+            Console.WriteLine($"Crocodiles lay { new Crocodile().LayEggs()} eggs.");
+            Console.WriteLine($"Snakes lay { new Snake().LayEggs()} eggs.");
+            Console.WriteLine();
+            Console.WriteLine("...and bears hibernate:");
+            new Bear().Hibernate(24);
+            Console.WriteLine();
+
+            // Demonstrate Domestic Behavior
+            Console.WriteLine("Domestic animals can be trained by their owners:");
+            Console.WriteLine(new Cat("Jerry Cat").TrainWith("You"));
+            Console.WriteLine(new Horse("Apple Jack").TrainWith("You"));
+            Console.WriteLine();
+
+            // Demonstrate Fantasy Behavior
+            Console.WriteLine("Fantasy creatures have magical powers!");
+            Console.WriteLine(new Dragon().DoMagic());
+            Console.WriteLine(new Phoenix().DoMagic());
+            Console.WriteLine("Age is kind of a joke to immortal phoenixes");
         }
     }
 }
